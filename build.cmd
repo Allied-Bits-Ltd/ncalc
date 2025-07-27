45ce1041ba344ef98befe4e7aa6b2cd6ef70c5b2
Eugene Mayevski <emayevski@users.noreply.github.com>
2025-07-27 20:55:48 +0200

    Updated the build script.

--- build.cmd
+++ build.cmd
@@ -2,37 +2,13 @@
 
 set MSBUILDRIR=C:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin
 
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Core\NCalc.Core.csproj  /p:Configuration=Release /p:TargetFramework=net462
+"%MSBUILDRIR%\msbuild.exe" src\NCalc\NCalc.csproj  /p:Configuration=Release /p:TargetFramework=net462
 if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Core\NCalc.Core.csproj  /p:Configuration=Release /p:TargetFramework=netstandard2.0
+"%MSBUILDRIR%\msbuild.exe" src\NCalc\NCalc.csproj  /p:Configuration=Release /p:TargetFramework=netstandard2.0
 if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Core\NCalc.Core.csproj  /p:Configuration=Release /p:TargetFramework=net8.0
+"%MSBUILDRIR%\msbuild.exe" src\NCalc\NCalc.csproj  /p:Configuration=Release /p:TargetFramework=net8.0
 if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Core\NCalc.Core.csproj  /p:Configuration=Release /p:TargetFramework=net9.0
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Sync\NCalc.Sync.csproj  /p:Configuration=Release /p:TargetFramework=net462
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Sync\NCalc.Sync.csproj  /p:Configuration=Release /p:TargetFramework=netstandard2.0
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Sync\NCalc.Sync.csproj  /p:Configuration=Release /p:TargetFramework=net8.0
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Sync\NCalc.Sync.csproj  /p:Configuration=Release /p:TargetFramework=net9.0
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Async\NCalc.Async.csproj /p:Configuration=Release /p:TargetFramework=net462
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Async\NCalc.Async.csproj /p:Configuration=Release /p:TargetFramework=netstandard2.0
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Async\NCalc.Async.csproj /p:Configuration=Release /p:TargetFramework=net8.0
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.Async\NCalc.Async.csproj /p:Configuration=Release /p:TargetFramework=net9.0
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.DependencyInjection\NCalc.DependencyInjection.csproj /p:Configuration=Release /p:TargetFramework=net462
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.DependencyInjection\NCalc.DependencyInjection.csproj /p:Configuration=Release /p:TargetFramework=netstandard2.0
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.DependencyInjection\NCalc.DependencyInjection.csproj /p:Configuration=Release /p:TargetFramework=net8.0
-if errorlevel 1 goto error
-"%MSBUILDRIR%\msbuild.exe" src\NCalc.DependencyInjection\NCalc.DependencyInjection.csproj /p:Configuration=Release /p:TargetFramework=net9.0
+"%MSBUILDRIR%\msbuild.exe" src\NCalc\NCalc.csproj  /p:Configuration=Release /p:TargetFramework=net9.0
 if errorlevel 1 goto error
 
 nuget pack NCalc.nuspec
