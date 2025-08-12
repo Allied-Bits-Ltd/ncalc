7184b3c0c8143d04c270559b8c0b558613965883
Eugene Mayevski <emayevski@users.noreply.github.com>
2025-08-12 15:32:43 +0200

    - EvaluationVisitor got a constructor that accepts an expression (so that it can pick the context from there).

--- src/NCalc/ExpressionBase.cs
+++ src/NCalc/ExpressionBase.cs
@@ -12,7 +12,7 @@ namespace NCalc;
 /// </summary>
 public abstract class ExpressionBase<TExpressionContext> where TExpressionContext : ExpressionContextBase, new()
 {
-    protected TExpressionContext Context { get; }
+    internal TExpressionContext Context { get; }
 
     /// <summary>
     /// Options for the expression evaluation.
